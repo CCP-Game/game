fc87c59ead95a15c7abf3dceed2f0870dc0022f3
nicbe305 <nicbe305@student.otago.ac.nz>
2024-08-06 11:20:04 +1200

    Added Point & Room classes

---
+++ Point.cpp
@@ -0,0 +1,29 @@
+#include "Point.h"
+
+Point::Point(int x , int y){
+    this->x=x;
+    this->y=y;
+};
+
+int Point::getX(){
+    return this->x;
+};
+
+int Point::getY(){
+    return this->y;
+};
+
+void Point::setX(int x){
+    this->x=x;
+};
+
+void Point::setY(int y){
+    this->y=y;
+};
+
+void Point::setXY(int x, int y){
+    setX(x);
+    setY(y);
+};
+
+
