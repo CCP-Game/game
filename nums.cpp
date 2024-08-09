019d5cc0c299aa060da6d5f048041a0994d06467
poale7 <162065345+poale7@users.noreply.github.com>
2024-08-09 14:15:23 +1200

    added deleted nums file back into repo

---
+++ nums.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+const char *ASCII_NUMBERS[] = {
+    "  ___  \n"
+    " / _ \\ \n"
+    "| | | |\n"
+    "| | | |\n"
+    "| |_| |\n"
+    " \\___/ \n",
+
+    " __ \n"
+    "/_ |\n"
+    " | |\n"
+    " | |\n"
+    " | |\n"
+    " |_|\n",
+
+    " ___  \n"
+    "|__ \\ \n"
+    "   ) |\n"
+    "  / / \n"
+    " / /_ \n"
+    "|____|\n",
+
+    " ____  \n"
+    "|___ \\ \n"
+    "  __) |\n"
+    " |__ < \n"
+    " ___) |\n"
+    "|____/ \n",
+
+    " _  _   \n"
+    "| || |  \n"
+    "| || |_ \n"
+    "|__   _|\n"
+    "   | |  \n"
+    "   |_|  \n",
+
+    " _____ \n"
+    "| ____|\n"
+    "| |__  \n"
+    "|___ \\ \n"
+    " ___) |\n"
+    "|____/ \n",
+
+    "   __  \n"
+    "  / /  \n"
+    " / /_  \n"
+    "| '_ \\ \n"
+    "| (_) |\n"
+    " \\___/ \n",
+
+    " ______ \n"
+    "|____  |\n"
+    "    / / \n"
+    "   / /  \n"
+    "  / /   \n"
+    " /_/    \n",
+
+    "  ___  \n"
+    " / _ \\ \n"
+    "| (_) |\n"
+    " > _ < \n"
+    "| (_) |\n"
+    " \\___/ \n",
+
+    "  ___  \n"
+    " / _ \\ \n"
+    "| (_) |\n"
+    " \\__, |\n"
+    "   / / \n"
+    "  /_/  \n",
+
+    "    _   \n"
+    "  _| |_ \n"
+    " |_   _|\n"
+    "   |_|  \n"
+    "        \n"
+    "        \n",
+
+    "         \n"
+    "         \n"
+    "  ______ \n"
+    " |______|\n"
+    "         \n"
+    "         \n",
+
+    "      __\n"
+    "     / /\n"
+    "    / / \n"
+    "   / /  \n"
+    "  / /   \n"
+    " /_/    \n",
+
+    "       \n"
+    " __  __\n"
+    " \\ \\/ /\n"
+    "  >  < \n"
+    " /_/\\_\\\n"
+    "       \n",
+
+    "         \n"
+    "  ______ \n"
+    " |______|\n"
+    "  ______ \n"
+    " |______|\n"
+    "         \n",
+
+};
+
+void printNums(const std::string &nums)
+{
+    // Prepare a vector to hold each line of the final ASCII art
+    std::vector<std::string> lines(6, "");
+
+    // Process each character in the input string
+    for (char ch : nums)
+    {
+        int index = -1;
+        if (ch >= '0' && ch <= '9')
+        {
+            index = ch - '0';
+        }
+        else if (ch == '+')
+        {
+            index = 10;
+        }
+        else if (ch == '-')
+        {
+            index = 11;
+        }
+        else if (ch == '/')
+        {
+            index = 12;
+        }
+        else if (ch == '*')
+        {
+            index = 13;
+        }
+        else if (ch == '=')
+        {
+            index = 14;
+        }
+        else if (ch == ' ')
+        {
+            // Add spaces between characters for readability
+            for (int i = 0; i < 6; ++i)
+            {
+                lines[i] += "  "; // Two spaces for separation
+            }
+            continue;
+        }
+
+        if (index != -1)
+        {
+            // Append the corresponding ASCII art to each line
+            const char *ascii = ASCII_NUMBERS[index];
+            std::string line;
+            int lineIndex = 0;
+
+            for (const char *p = ascii; *p; ++p)
+            {
+                if (*p == '\n')
+                {
+                    lines[lineIndex] += line + ' ';
+                    line.clear();
+                    ++lineIndex;
+                }
+                else
+                {
+                    line += *p;
+                }
+            }
+        }
+    }
+
+    // Print each line of the final ASCII art
+    for (const std::string &line : lines)
+    {
+        std::cout << line << std::endl;
+    }
+}
+
+int main()
+{
+    // std::string input = "+-/*11 + 19 - 3 / 2 * 4";
+
+    std::string input = "=+-/*123=4562343253509876541321";
+    printNums(input);
+    return 0;
+}
