1f8b46f24b4f49932aa52baa324b24372d986322
poale7 <162065345+poale7@users.noreply.github.com>
2024-08-17 12:54:50 +1200

    Added a .gitignore to hopefully clear up the repo and I am alos testing to see if the actually having a test.cpp file works with github actions

--- Codebase/Test.cpp
+++ Codebase/Test.cpp
@@ -3,8 +3,10 @@
 #include "Room.h"
 #include "Player.h"
 #include <iostream>
+#include <cassert>
 int main(){
     Pos p1(10,10);
-    std::cout<< p1.getX()<< "\n";
+    assert(p1.getX() == 10);
+    assert(p1.getY() == 10);
     return 0; 
 }
