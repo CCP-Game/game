97592b014b1a4c10f5c3c9a07e1513df17f1082c
poale7 <162065345+poale7@users.noreply.github.com>
2024-08-23 14:04:14 +1200

    commenting out some of Pos.h to see if ci works

--- Codebase/Pos.h
+++ Codebase/Pos.h
@@ -13,23 +13,23 @@ public:
     void setX(int x);
     void setY(int y);
 
-    // Equality operator
-    bool operator==(const Pos &other) const
-    {
-        return this->getX() == other.getX() && this->getY() == other.getY();
-    }
+    // // Equality operator
+    // bool operator==(const Pos &other) const
+    // {
+    //     return this->getX() == other.getX() && this->getY() == other.getY();
+    // }
 
-    // Hash function (see step 2)
-    struct HashFunction
-    {
-        std::size_t operator()(const Pos &p) const
-        {
-            // Compute individual hash values for x and y and combine them
-            std::size_t h1 = std::hash<int>{}(p.getX());
-            std::size_t h2 = std::hash<int>{}(p.getY());
-            return h1 ^ (h2 << 1); // or use boost::hash_combine
-        }
-    };
+    // // Hash function (see step 2)
+    // struct HashFunction
+    // {
+    //     std::size_t operator()(const Pos &p) const
+    //     {
+    //         // Compute individual hash values for x and y and combine them
+    //         std::size_t h1 = std::hash<int>{}(p.getX());
+    //         std::size_t h2 = std::hash<int>{}(p.getY());
+    //         return h1 ^ (h2 << 1); // or use boost::hash_combine
+    //     }
+    // };
 
 private:
     int x;
